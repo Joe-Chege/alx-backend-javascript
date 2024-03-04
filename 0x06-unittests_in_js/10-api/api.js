3c6a0763a2ce51c6d611d3e90196487150919962
Joe-Chege <njorogejosephchege@gmail.com>
2024-03-04 09:35:25 +0000

    adding the utils.js

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,22 +1,40 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
+const port = 7865;
 
-app.listen(7865, () => {
-  console.log("API available on localhost port 7865");
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the payment system');
 });
 
-app.get("/available_payments", (req, res) => {
-  res.json({
-    payment_methods: {
-      credit_cards: true,
-      paypal: false,
-    },
-  });
+app.get('/cart/:id([0-9]+)', (request, response) => {
+    response.send(`Payment methods for cart ${request.params.id}`);
 });
 
-app.use(express.json()); // Enable parsing JSON in requests
-
-app.post("/login", (req, res) => {
-  const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+app.get('/available_payments', (request, response) => {
+    response.set("Content-Type", "application/json");
+    const payMethods = {
+	payment_methods: {
+          credit_cards: true,
+          paypal: false
+	}
+    }
+    response.send(payMethods);
 });
+
+app.post('/login', (request, response) => {
+    const userName = request.body.userName;
+    if (userName) {
+	response.send(`Welcome ${userName}`);
+    } else {
+	response.status(404).send();
+    }
+});
+
+app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
